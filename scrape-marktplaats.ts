902108c8476551a576b9f9c52fa478b7f4315219
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2025-09-01 10:45:17 +0200

    scraping task 1

--- scrape-marktplaats.ts
+++ scrape-marktplaats.ts
@@ -9,8 +9,31 @@ async function main() {
   const page = await browser.newPage();
   await page.goto("https://www.marktplaats.nl/");
 
-  const a = 5;
-  console.log(a);
+  const listingUrls = await page.$$eval("a[href^='/v/']", (links) =>
+    links.map((link) => (link as HTMLAnchorElement).href)
+  );
+
+  console.log("listingUrls", listingUrls);
+
+  for (const url of listingUrls) {
+    const listingPage = await browser.newPage();
+    await listingPage.goto(url, { waitUntil: "domcontentloaded" });
+
+    const data = await listingPage.evaluate(() => {
+      const name = document.querySelector("h1")?.textContent?.trim() || "N/A";
+      const price =
+        document
+          .querySelector("div.ListingHeader-price")
+          ?.textContent?.trim() || "N/A";
+      console.log("oo", price);
+
+      return { name, price };
+    });
+    console.log("---", data);
+    await listingPage.close();
+  }
+
+  await browser.close();
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
