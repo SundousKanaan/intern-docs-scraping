a6b3c9e0d1a09adb58570e48103dca3ac82d729f
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2025-09-08 10:38:03 +0200

    Refactor Marktplaats scraping logic to use keyword & page number

--- platforms/marktplaats.ts
+++ platforms/marktplaats.ts
@@ -5,71 +5,36 @@ export class Marktplaats implements Platform {
   name = "marktplaats";
 
   async scrapeSearchPage(
-    page: Page,
+    _page: Page,
     keyword: string,
     limit: number
   ): Promise<string[]> {
+    const browser = await puppeteer.launch({
+      headless: false,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
     let pageNumber = 1;
     const urls: string[] = [];
 
     while (urls.length < limit) {
       const url = `https://www.marktplaats.nl/q/${keyword}/p/${pageNumber}/`;
-      await page.goto(url);
-      const items = await page.$$eval("a.hz-Listing-coverLink", (items) => {
-        return items.map((item) => {
-          return "https://www.marktplaats.nl" + item.href;
+
+      await page.goto(url, { waitUntil: "domcontentloaded" });
+
+      // "a.hz-Listing-coverLink" with this selector i get some empty results
+      // a[href^='/v/']
+      const items = await page.$$eval("a[href^='/v/']", (links) => {
+        return links.map((link) => {
+          // "https://www.marktplaats.nl" +
+          return link.href;
         });
       });
       urls.push(...items);
       pageNumber++;
     }
-    return urls;
-    // try {
-    //   console.log("start scrapeSearchPage");
-    //   const browser = await puppeteer.launch({
-    //     headless: false,
-    //     args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    //   });
-    //   const page = await browser.newPage();
-    //   await page.goto("https://www.marktplaats.nl/", {
-    //     waitUntil: "domcontentloaded",
-    //   });
-    //   // close cookies popup
-    //   const iframeElement = await page.$("#sp_message_iframe_1278200");
-    //   if (iframeElement) {
-    //     const iframContent = await iframeElement.contentFrame();
-    //     try {
-    //       const btn = await iframContent?.$("button.sp_choice_type_SE");
-    //       if (btn) await btn.click();
-    //       console.log("Cookies popup closed");
-    //       await new Promise((resolve) => setTimeout(resolve, 1000));
-    //     } catch (error) {
-    //       console.error(error);
-    //     }
-    //   }
-    //   let listingUrls: string[] = [];
-    //   let scrollTries = 0;
-    //   while (listingUrls.length < limit && scrollTries < 30) {
-    //     const newUrls = await page.$$eval("a[href^='/v/']", (links) =>
-    //       links.map((link) => (link as HTMLAnchorElement).href)
-    //     );
-    //     listingUrls = Array.from(new Set([...listingUrls, ...newUrls]));
-    //     if (listingUrls.length >= limit || newUrls.length === limit) {
-    //       break;
-    //     }
-    //     await page.evaluate(() => {
-    //       window.scrollBy(0, window.innerHeight);
-    //     });
-    //     await new Promise((resolve) => setTimeout(resolve, 1500));
-    //     scrollTries++;
-    //   }
-    //   await browser.close();
-    //   console.log("Finish scrapeSearchPage");
-    //   return listingUrls.slice(0, limit);
-    // } catch (e) {
-    //   console.error(e);
-    //   return [];
-    // }
+    await browser.close();
+    return urls.slice(0, limit);
   }
 
   async scrapeItemPage(page: Page): Promise<Listing> {
--- scrape-marktplaats.ts
+++ scrape-marktplaats.ts
@@ -1,20 +1,53 @@
 import puppeteer from "puppeteer";
 
 async function main() {
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-
-  const page = await browser.newPage();
-  await page.goto("https://www.marktplaats.nl/");
-
-  const a = 5;
-  console.log(a);
-
-  // some comments to show they are gone.
-  // some more
-  // even more
+  const limit = 1; // temp limit
+  try {
+    console.log("start scrapeSearchPage");
+    const browser = await puppeteer.launch({
+      headless: false,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
+    await page.goto("https://www.marktplaats.nl/", {
+      waitUntil: "domcontentloaded",
+    });
+    // close cookies popup
+    const iframeElement = await page.$("#sp_message_iframe_1278200");
+    if (iframeElement) {
+      const iframContent = await iframeElement.contentFrame();
+      try {
+        const btn = await iframContent?.$("button.sp_choice_type_SE");
+        if (btn) await btn.click();
+        console.log("Cookies popup closed");
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    let listingUrls: string[] = [];
+    let scrollTries = 0;
+    while (listingUrls.length < limit && scrollTries < 30) {
+      const newUrls = await page.$$eval("a[href^='/v/']", (links) =>
+        links.map((link) => (link as HTMLAnchorElement).href)
+      );
+      listingUrls = Array.from(new Set([...listingUrls, ...newUrls]));
+      if (listingUrls.length >= limit || newUrls.length === limit) {
+        break;
+      }
+      await page.evaluate(() => {
+        window.scrollBy(0, window.innerHeight);
+      });
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      scrollTries++;
+    }
+    await browser.close();
+    console.log("Finish scrapeSearchPage");
+    return listingUrls.slice(0, limit);
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
